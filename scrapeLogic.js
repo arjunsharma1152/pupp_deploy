fa9fc8640fc88c2faa737ca1894862d51dcb4ae9
Arjun Sharma <arjunsk923@gmail.com>
2023-11-06 22:28:30 +0530

    second commit

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,29 +17,77 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
-
-    // Set screen size
+    await page.goto('https://www.codechef.com/contests?itm_medium=navmenu');
+    
     await page.setViewport({ width: 1080, height: 1024 });
 
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    const codechefItems = [];
 
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    await page.waitForSelector("#root > div > div > div > div > div > div:nth-child(2) > div > div > div > table > tbody > tr > td:nth-child(2) > div > a > span",1200000);
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    const codechefName = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#root > div > div > div > div > div > div:nth-child(2) > div > div > div > table > tbody > tr > td:nth-child(2) > div > a > span')).map(x => x.textContent)) });
 
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    const codechefDate = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#root > div > div > div > div > div > div:nth-child(2) > div > div > div > table > tbody > tr > td:nth-child(3) > div > div > div')).map(x => x.textContent)) });
+
+    const codechefDuration = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#root > div > div > div > div > div > div:nth-child(2) > div > div > div > table > tbody > tr > td:nth-child(4) > div > div > p')).map(x => x.textContent)) });
+
+    const codechefStartsIn = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#root > div > div > div > div > div > div:nth-child(2) > div > div > div > table > tbody > tr > td:nth-child(5) > div > div > p:nth-child(1)')).map(x => x.textContent)) });
+
+    for (let i = 0; i < codechefName.length; i++) {
+
+        const obj = {
+            name: codechefName[i],
+            date: codechefDate[i],
+            duration: codechefDuration[i],
+            startsIn: codechefStartsIn[i]
+        }
+        codechefItems.push(obj);
+    }
+
+    // CODEFORCES
+
+    await page.goto('https://codeforces.com/contests');
+
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    const codeforcesItems = [];
+
+    const codeforcesName = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#pageContent > div.contestList > div.datatable > div > table > tbody > tr > td.left')).map(x => x.textContent)) });
+
+    const codeforcesDate = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#pageContent > div.contestList > div.datatable > div > table > tbody > tr > td > a:not([class])')).map(x => x.innerHTML)) });
+
+    const codeforcesDuration = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#pageContent > div.contestList > div.datatable > div > table > tbody > tr > td:nth-child(4)')).map(x => x.textContent)) });
+
+    const codeforcesStartsIn = await page.evaluate(() => { return (Array.from(document.querySelectorAll('#pageContent > div.contestList > div.datatable > div > table > tbody > tr > td.state > span > span')).map(x => x.textContent)) });
+
+    await browser.close();
+
+    for (let i = 0; i < codechefName.length; i++) {
+
+        const obj = {
+            name: codeforcesName[i].replace(/\n/g, "").trim(),
+            date: codeforcesDate[i].replace(/\n/g, "").trim(),
+            duration: codeforcesDuration[i].replace(/\n/g, "").trim(),
+            startsIn: codeforcesStartsIn[i]
+        }
+        codeforcesItems.push(obj);
+    }
+
+    const fullData = [];
+
+    fullData.push({
+        platform: "codechef",
+        contests: codechefItems
+    });
+
+    fullData.push({
+        platform: "codeforces",
+        contests: codeforcesItems
+    });
+
+    const data = JSON.stringify(fullData);
+
+    res.send(data);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
